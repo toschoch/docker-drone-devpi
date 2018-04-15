74fedaf08917180ac788869750aa4d9a6f77d62a
Tobias Schoch <tobias.schoch@vtxmail.ch>
2018-04-15 02:51:01 +0200

    fixed upload

--- script.sh
+++ script.sh
@@ -4,20 +4,23 @@ devpi login ${PLUGIN_USERNAME} --password=${PLUGIN_PASSWORD}
 devpi use ${PLUGIN_SERVER}/${PLUGIN_INDEX}
 #python3 setup.py bdist_wheel
 
+echo upload wheels to server
 for i in dist/*.whl; do
-    echo upload wheel to server
     devpi upload $i
 done
 
+echo upload zips to server
 for i in dist/*.zip; do
-    echo upload zip to server
     devpi upload $i
 done
 
+echo upload tarballs to server
 for i in dist/*.tar.gz; do
-    echo upload tarballs to server
     devpi upload $i
 done
+
+exit 0 
+
 
 
 
